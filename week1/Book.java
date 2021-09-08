7a6bd3e1759a5f0c18ac5b7e941a05a94765e841
Mohamed Alie Pussah, II <talk2alie@outlook.com>
2021-09-08 13:26:56 -0400

    Add new stuff from week 1

---
+++ week1/Book.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+/**
+ * This is the description of the class
+ */
+public class Book {
+    /**
+     * This is the description of the method
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("This is my book.");
+        System.out.print("The author " +
+          "of the book is ");
+        System.out.print("Sheldon Cooper");
+        System.out.println();
+        System.out.println(125);
+        System.out.println(25 * 4 / 2);
+        // This is for stuff
+        Scanner keyboard = new Scanner(System.in);
+    }
+}
--- week1/HelloVillanova.java
+++
@@ -1,5 +0,0 @@
-public class HelloVillanova {
-    public static void main(String[] args) {
-       System.out.println("Hello, Villanova!"); 
-    }
-}
