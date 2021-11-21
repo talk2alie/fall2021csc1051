df11f3374449b605242a533f73b2c2318fc43727
Mohamed Alie Pussah, II <talk2alie@outlook.com>
2021-11-21 16:41:32 -0500

    Add final touches to project

--- projects/FinalProject/app/ConsoleTodoApp.java
+++ projects/FinalProject/app/ConsoleTodoApp.java
@@ -1,33 +1,11 @@
 package app;
 
-import java.io.IOException;
-import java.util.Scanner;
-
-import activities.TodoItem;
-import activities.TodoList;
-import data.Database;
-
 public class ConsoleTodoApp {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         System.out.println();
 
-        final String EXIT = "exit";
-
-        Scanner scanner = new Scanner(System.in);
-        String command = EXIT;
-
-        do {
-            System.out.println("Welcome to Console To Do");
-            System.out.println("To continute, please use the following commands:");
-            System.out.println("1. vl: To view all To Do Lists and their items");
-            System.out.println("2. vl <Todo List Title>: For e.g. \"vl My Day\" to view a To Do list, with the title \"My Day\"");
-            System.out.println("3. al: To add a new To Do List");
-            System.out.println("3. ai <Todo List Title>: For e.g. \"ai My Day\" to add a new To Do Item to a To Do list, with title \"My Day\"");
-            System.out.println("3. ri <Todo List Title>: For e.g. \"ri My Day\" to remove a To Do Item from a To Do list, with title \"My Day\"");
-            System.out.println("3. ai <Todo List Title>: For e.g. \"ai My Day\" to add a new To Do Item to a To Do list, with title \"My Day\"");
-
-        } while(!command.equalsIgnoreCase(EXIT));
-        scanner.close();
+        // Write code here that shows an example of using the Database class
+        // Make sure to use try-catch and incude the Scanner class somewhere
 
         System.out.println();
     }
